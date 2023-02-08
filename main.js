988a7d6def67a639002ce9e68b4317cb13139bba
Karolína Lauferová <duskova.karolina@email.cz>
2023-02-08 10:59:29 +0100

    hotovo

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 // tady je místo pro náš program
 
+//Jmeno Prijmeni a vek
+let Jmeno = prompt("Jake je Vase krestni jmeno?");
+let Prijmeni = prompt("Jake je Vase prijmeni?");
+let Narozen = prompt("Jaky je Vas rok narozeni?");
+let Colour = prompt("What is your favourite colour?");
 
+document.body.innerHTML += "<p id=odstavec>" + Jmeno + " " + Prijmeni + ", vek: " + (2023-Narozen) + "</p>";
+
+odstavec.style.color = Colour 
 
 
